887a300caf018eeb2ac479f2a09c41678f25288e
Reine0017 <m.fortitudo.fr@gmail.com>
2020-06-14 14:57:46 +0700

    display artist images

--- src/components/Home/artistsList.js
+++ src/components/Home/artistsList.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const ArtistsList = (props) => {
-  console.log(props);
+const ArtistsList = ({ allArtists }) => {
+  allArtists ? console.log(allArtists) : console.log("nothing yet");
+
+  const listFn = (values) =>
+    values
+      ? values.map((item) => (
+          <Link
+            key={item.id}
+            to={`/artist/${item.id}`}
+            className="artist_item"
+            style={{ background: `url('/images/covers/${item.cover}.jpg')` }}
+          >
+            <div>{item.name}</div>
+          </Link>
+        ))
+      : null;
   return (
     <div className="artists_list">
       <h4>Browse the artists</h4>
-      <div className="artist_container"></div>
+      <div className="artist_container">{listFn(allArtists)}</div>
     </div>
   );
 };
